714fb707975c2a8371245d838a4fe402eaddca8f
mguazzardo <mguazzardo76@gmail.com>
2021-06-04 12:07:11 -0300

    Create paso02.sh

---
+++ paso02.sh
@@ -0,0 +1,3 @@
+oc create -f https://raw.githubusercontent.com/tektoncd/catalog/master/task/openshift-client/0.1/openshift-client.yaml
+oc create -f https://raw.githubusercontent.com/openshift/pipelines-catalog/master/task/s2i-nodejs/0.1/s2i-nodejs.yaml
+oc create -f https://raw.githubusercontent.com/openshift/pipelines-catalog/master/task/s2i-php/0.1/s2i-php.yaml
